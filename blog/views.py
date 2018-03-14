87a570c58742e159f98921e4c95e8e8ea50a020c
gangler <buhuo1996@gmail.com>
2018-03-14 20:24:27 +0800

    Modified posts models

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ from .models import Post
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': post})
