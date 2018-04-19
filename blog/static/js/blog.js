01582dc22e5ca1d6949c6c87dac3d8f9d945e015
gangler <buhuo1996@gmail.com>
2018-04-19 20:36:53 +0800

    increase the module of delete blog

---
+++ blog/static/js/blog.js
@@ -0,0 +1,6 @@
+function delete_click(){
+
+    if(confirm('确定删除？') == true){
+        document.getElementById('a-delete').href = "delete";
+    }
+}
--- blog/views.py
+++ blog/views.py
@@ -40,6 +40,9 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
-
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
 
 
